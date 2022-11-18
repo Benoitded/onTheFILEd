cde43dd599c59d350e05aaa82132bb2215316532
rmazouz <rmazouz@orika.fr>
2022-11-18 20:44:21 +0100

    Ajout des routers Début du tableau

---
+++ frontend-react/src/components/TableComponent/TableComponent.jsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number,
+) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('France', 159, 6.0, 24),
+  createData('Brasil', 237, 9.0, 37),
+  createData('Argentina', 262, 16.0, 24),
+  createData('Germany', 305, 3.7, 67),
+  createData('Spain', 356, 16.0, 49),
+];
+
+export default function BasicTable() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Team Name</TableCell>
+            <TableCell align="right">TVL</TableCell>
+            <TableCell align="right">Fans Number</TableCell>
+            <TableCell align="right">Win rate</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
---
+++ frontend-react/src/pages/TablePage/TablePage.jsx
@@ -0,0 +1,12 @@
+import TableComponent from "../../components/TableComponent/TableComponent.jsx";
+import React, { useEffect, useState } from "react";
+
+function App() {
+  return (
+  <div>
+    <TableComponent/>
+  </div>
+  );
+}
+
+export default App;
