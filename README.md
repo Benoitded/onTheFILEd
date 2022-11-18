fe95591f16ea87e0b76522617f0bcb1c768bb9e3
Benoit <benoitded82@hotmail.fr>
2022-11-18 18:59:27 +0100

    Start project

---
+++ README.md
@@ -0,0 +1,38 @@
+# onTheFILd
+
+Project for the fEVM Hackathon
+
+## Quick start front
+
+- Clone the git
+  `git clone https://github.com/Benoitded/onTheFILd.git`
+
+- Go in the folder
+  `cd onTheFILd`
+
+- Go in the front folder
+  `cd frontend-react`
+
+- Install missing dependencies
+  `npm install`
+
+- Launch the dApp
+  `npm start`
+
+- Here you go!
+
+## Quick start contracts
+
+- Go in the folder
+  `cd onTheFILd`
+
+- Install hardhat files
+  `npm install --save dotenv`
+
+- Modify the contract(s)
+  in `contracts/newContracts`
+
+- Modify the run script then run it (or deploy script if ready)
+  `npx hardhat run scripts/run.js`
+
+> Not ready yet, but it should update automaticly the abi and contract
---
+++ scripts/run.js
@@ -0,0 +1,28 @@
+const main = async() => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
+    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+    const waveContract = await waveContractFactory.deploy();
+    await waveContract.deployed();
+
+    console.log("Contract deployed to:", waveContract.address);
+    console.log("Contract deployed by:", owner.address);
+
+    await waveContract.getTotalWaves();
+
+    const waveTxn = await waveContract.wave();
+    await waveTxn.wait();
+
+    await waveContract.getTotalWaves();
+};
+
+const runMain = async() => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+runMain();
