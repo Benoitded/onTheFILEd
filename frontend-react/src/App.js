c9d18ee68ee6bc867e1f954d4effe9b275222080
Benoit <benoitded82@hotmail.fr>
2022-11-19 19:47:16 +0100

    Added swap app

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -2,11 +2,6 @@ import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header/Header.jsx";
-import Swap from "./components/Swap/Swap.jsx";
-import Headline from "./components/Headline/Headline.jsx";
-import SelectCountry from "./components/SelectCountry/SelectCountry.jsx";
-import Bracket from "./components/Bracket/Bracket.jsx";
-import InfoTeam from "./components/InfoTeam/InfoTeam.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import HomePage from "./pages/HomePage/HomePage.jsx";
 import CountryPage from "./pages/CountryPage/CountryPage.jsx";
@@ -22,28 +17,18 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Swap />
         <Header
           currentAccount={currentAccount}
           setCurrentAccount={setCurrentAccount}
           chain={chain}
           setChain={setChain}
         />
-        <main>
-          <InfoTeam dataCountries={dataCountries[1]} isHolder={false} />
-          <Headline />
-          <SelectCountry />
-          <Bracket />
-        </main>
-
-        <Link to="/table">Table</Link>
-
-        <Routes>
-          <Route path="/" component={{}} />
-          <Route path="/table" element={<TablePage />} />
-        </Routes>
-
-        <Footer />
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/:CountryName" element={<CountryPage />} />
+          </Routes>
+        </div>
       </Router>
       <Footer />
     </div>
